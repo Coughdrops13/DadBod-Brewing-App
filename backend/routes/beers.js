e65d6fe131d252b85535fce6e18b45ab1fea257e
Connor Roberts <connoobets@gmail.com>
2022-06-22 13:22:06 -0600

    add RESTful routes for beers

--- backend/routes/beers.js
+++ backend/routes/beers.js
@@ -1,8 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ mssg: "Get all beers" });
+// GET all beers
+router.get("/", (req, res) => {
+  res.json({ mssg: "GET all beers" });
+});
+
+// GET a single beer
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET a single beer" });
+});
+
+// POST a single beer
+router.post("/", (req, res) => {
+  res.json({ mssg: "POST a single beer" });
+});
+
+// DELETE a single beer
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE a single beer" });
+});
+
+// UPDATE a single beer
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "UPDATE a single beer" });
 });
 
 module.exports = router;
