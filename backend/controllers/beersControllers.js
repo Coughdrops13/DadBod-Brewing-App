fd7a2dd1f61b248dd95ab37e5df5f970a05bb447
Connor Roberts <connoobets@gmail.com>
2022-07-05 15:08:59 -0600

    finish beerController file

--- backend/controllers/beersControllers.js
+++ backend/controllers/beersControllers.js
@@ -15,13 +15,13 @@ const getBeer = async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({error: "No such beer."})
+    return res.status(404).json({ error: "No such beer." });
   }
 
   const beer = await Beer.findById(id);
 
   if (!beer) {
-    res.status(404).json({ error: "No such beer." });
+    return res.status(404).json({ error: "No such beer." });
   }
 
   res.status(200).json(beer);
@@ -33,18 +33,50 @@ const createBeer = async (req, res) => {
   const emptyFieldsArray = emptyFields([title, variety, abv, img]);
 
   if (emptyFieldsArray.length > 0) {
-    return res.status(400).json({error: "Please fill all required fields", emptyFieldsArray})
+    return res
+      .status(400)
+      .json({ error: "Please fill all required fields", emptyFieldsArray });
   }
 
   try {
     const beer = await Beer.create({ title, variety, abv, description, img });
     res.status(200).json(beer);
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
 };
 // DELETE a single beer
+const deleteBeer = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such beer to be deleted." });
+  }
+
+  const beer = await Beer.findOneAndDelete({ _id: id });
+
+  if (!beer) {
+    return res.status(404).json({ error: "No such beer to be deleted." });
+  }
+
+  res.status(200).json(beer);
+};
 // UPDATE a single beer
+const updateBeer = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Cannot update non-existant beer." });
+  }
+
+  const beer = await Beer.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!beer) {
+    return res.status(404).json({error: "Cannot update non-existant beer."})
+  }
+  
+  res.status(200).json(beer);
+}
 
 module.exports = {
   getBeers,
