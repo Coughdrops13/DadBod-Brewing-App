96c53ae2ca2d853dfc0b3cef6f09b464ce2fbf67
Connor Roberts <connoobets@gmail.com>
2022-07-07 15:34:35 -0600

    create getBeersList reducer in beerSlice to be dispatched from Beer component;

--- frontend/src/components/Beers.js
+++ frontend/src/components/Beers.js
@@ -1,10 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react'; 
   
 import classes from './Beers.module.css';
 
 const Beers = (props) => {
   const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchBeers = async () => {
+      const response = await fetch('/DadBod/beers')
+      const jsonBeers = await response.json();
+    }
+
+    fetchBeers();
+  }, [dispatch])
 };
 
 export default Beers;
--- frontend/src/store/beer-slice.js
+++ frontend/src/store/beer-slice.js
@@ -1,13 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialBeerState = {
-  inventory: {},
+  inventory: [],
 };
 
 const beerSlice = createSlice({
   name: 'beers',
   initialState: initialBeerState,
   reducers: {
+    getBeersList(state, action) {
+      state.inventory = [],
+      state.inventory.concat(action.payload)
+    },
     addBeer(state, action) {
       const newBeer = action.payload;
       const existingBeer = state.beers.inventory.find(beer => beer.name === newBeer.name);
