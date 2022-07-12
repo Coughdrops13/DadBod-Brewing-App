5ea1df9ad97fd0ad2f75b515a68cd2abc4f4e09f
Connor Roberts <connoobets@gmail.com>
2022-07-12 11:34:10 -0600

    add react-router logic and comment it out

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+// import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import store from './store/index';
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    {/* <BrowserRouter> */}
+      <App />
+    {/* </BrowserRouter> */}
   </Provider>
 );
-
